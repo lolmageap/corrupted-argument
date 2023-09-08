9e2114e5dfcd94865843e35b57a6293e24f57afb
lolmageap <ekxk1234@gmail.com>
2023-09-08 11:54:05 +0900

    BaseEntity 설정

---
+++ src/main/kotlin/gangnam/sft/corruptedargument/config/JpaConfig.kt
@@ -0,0 +1,11 @@
+package gangnam.sft.corruptedargument.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+
+}
---
+++ src/main/kotlin/gangnam/sft/corruptedargument/util/BaseEntity.kt
@@ -0,0 +1,43 @@
+package gangnam.sft.corruptedargument.util
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @CreatedDate
+    var createdAt : LocalDateTime? = null,
+
+    @LastModifiedDate
+    var updatedAt : LocalDateTime? = null
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        assert (other is BaseEntity) {"Invalid instance has passed."}
+        other as BaseEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+}
