5ebd605192e23cf7f209fdd53df4db7a41463321
lolmageap <ekxk1234@gmail.com>
2023-09-11 12:46:31 +0900

    UserResponse 타입으로 반환

--- src/main/kotlin/gangnam/sft/corruptedargument/domain/user/UserService.kt
+++ src/main/kotlin/gangnam/sft/corruptedargument/domain/user/UserService.kt
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service
 @Service
 class UserService(private val userRepository: UserRepository) {
 
-    fun save(userRequest: UserRequest) {
-        userRepository.save(userRequest.toUser())
-    }
+    fun save(userRequest: UserRequest) : UserResponse =
+        userRepository.save(userRequest.toUser()).let(UserResponse::toUserResponse)
 
 }
