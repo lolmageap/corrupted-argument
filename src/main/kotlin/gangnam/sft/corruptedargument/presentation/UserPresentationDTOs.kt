6d55a66a4f3ecae4ea97a9bb0d738839eb2702a9
lolmageap <ekxk1234@gmail.com>
2023-09-19 19:19:53 +0900

    validation 재설정

--- src/main/kotlin/gangnam/sft/corruptedargument/presentation/UserPresentationDTOs.kt
+++ src/main/kotlin/gangnam/sft/corruptedargument/presentation/UserPresentationDTOs.kt
@@ -4,12 +4,9 @@ import gangnam.sft.corruptedargument.service.UserRequest
 import gangnam.sft.corruptedargument.service.UserUpdate
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotEmpty
 import jakarta.validation.constraints.Past
 import jakarta.validation.constraints.Pattern
 import java.time.LocalDate
-
-
 
 data class SignUp(
 
@@ -22,22 +19,25 @@ data class SignUp(
     @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{6,16}$")
     val confirmPassword: String,
 
-    @field:NotEmpty(message = "이름은 필수입니다.")
-    val name: String?,
+    @field:NotBlank
+    val name: String,
 
     @field:Past
     var birth: LocalDate?,
 ) {
-    fun toUserRequest() : UserRequest {
+    init {
         require(password == confirmPassword) {"비밀번호 재입력 오류"}
+    }
 
+    fun toUserRequest() : UserRequest {
         return UserRequest(
-            name = this.name!!,
+            name = this.name,
             password = this.password,
             email = this.email,
             birth = this.birth,
         )
     }
+
 }
 
 data class UserEdit(
