cfe226e0a468849101a617732195fc586a6475b1
lolmageap <ekxk1234@gmail.com>
2023-10-17 14:50:18 +0900

    페이지네이션 default 1로 설정

--- src/main/kotlin/gangnam/sft/corruptedargument/config/JpaConfig.kt
+++ src/main/kotlin/gangnam/sft/corruptedargument/config/JpaConfig.kt
@@ -1,9 +1,20 @@
 package gangnam.sft.corruptedargument.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer
 
 
 @Configuration
 @EnableJpaAuditing
-class JpaConfig
+class JpaConfig {
+
+    @Bean
+    fun customize(): PageableHandlerMethodArgumentResolverCustomizer? {
+        return PageableHandlerMethodArgumentResolverCustomizer {
+            it.setOneIndexedParameters(true)
+        }
+    }
+
+}
