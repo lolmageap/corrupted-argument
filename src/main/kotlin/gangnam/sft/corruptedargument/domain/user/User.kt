ff0ff04b4df6cbfd68841357ca32b145d513679b
lolmageap <ekxk1234@gmail.com>
2023-10-17 15:45:42 +0900

    user entity 추가 설정

--- src/main/kotlin/gangnam/sft/corruptedargument/domain/user/User.kt
+++ src/main/kotlin/gangnam/sft/corruptedargument/domain/user/User.kt
@@ -3,9 +3,11 @@ package gangnam.sft.corruptedargument.domain.user
 import gangnam.sft.corruptedargument.util.BaseDeleteEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
+import org.hibernate.annotations.SQLDelete
 import java.time.LocalDate
 
 @Entity
+@SQLDelete(sql = "update user set deleted = true where id = ?")
 @Table(name = "users")
 class User(
     var name: String,
