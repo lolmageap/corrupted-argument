91eb40e2317b7aeff8961fa94258e0d32c3c7d2a
lolmageap <ekxk1234@gmail.com>
2023-10-17 14:51:20 +0900

    반환타입 unit 삭제

--- src/main/kotlin/gangnam/sft/corruptedargument/service/UserService.kt
+++ src/main/kotlin/gangnam/sft/corruptedargument/service/UserService.kt
@@ -2,7 +2,9 @@ package gangnam.sft.corruptedargument.service
 
 import gangnam.sft.corruptedargument.annotation.WriteService
 import gangnam.sft.corruptedargument.domain.user.UserRepository
+import gangnam.sft.corruptedargument.exception.Domain
 import gangnam.sft.corruptedargument.exception.ExistException
+import gangnam.sft.corruptedargument.exception.NotFoundException
 import gangnam.sft.corruptedargument.exception.Property.EMAIL
 import gangnam.sft.corruptedargument.presentation.SignIn
 import gangnam.sft.corruptedargument.presentation.UserEdit
@@ -11,16 +13,16 @@ import org.springframework.data.repository.findByIdOrNull
 @WriteService
 class UserService(private val userRepository: UserRepository) {
 
-    fun signUp(userRequest: UserRequest): Unit {
+    fun signUp(userRequest: UserRequest) {
         emailDuplicationCheck(userRequest.email)
         userRepository.save(userRequest.toUser())
     }
 
-    fun signIn(signIn: SignIn): Unit {
+    fun signIn(signIn: SignIn) {
 
     }
 
-    fun editUser(userId: Long, userEdit: UserEdit): Unit {
+    fun editUser(userId: Long, userEdit: UserEdit) {
         userRepository.findByIdOrNull(userId)
             ?.update(name = userEdit.name, birth = userEdit.birth)
             ?: throw IllegalArgumentException()
